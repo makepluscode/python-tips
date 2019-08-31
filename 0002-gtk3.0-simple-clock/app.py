65fe0e25768d08ae125956e14cfae48184f48d1b
makepluscode <39910774+makepluscode@users.noreply.github.com>
2019-08-31 12:02:54 -0700

    Create app.py

---
+++ 0002-gtk3.0-simple-clock/app.py
@@ -0,0 +1,40 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gdk
+
+import time, datetime
+
+class Window(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Label Example")
+
+        self.modify_bg(Gtk.StateType.NORMAL, Gdk.Color(0xFFFF, 0xFFFF, 0xFFFF))
+
+        hbox = Gtk.Box(spacing=80)
+        hbox.set_homogeneous(False)
+        vbox_left = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=80)
+        vbox_left.set_homogeneous(False)
+        vbox_right = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=80)
+        vbox_right.set_homogeneous(False)
+
+        hbox.pack_start(vbox_left, True, True, 0)
+        hbox.pack_start(vbox_right, True, True, 0)
+
+        self.label11 = Gtk.Label('<span size="50000">' + str(datetime.datetime.now().date()) + '</span>')
+        self.label11.set_use_markup(True)
+        self.label11.set_line_wrap(True)
+        vbox_left.pack_start(self.label11, True, True, 0)
+
+        self.label21 = Gtk.Label('<span size="50000">' + str(datetime.datetime.now().time()) + '</span>')
+        self.label21.set_use_markup(True)
+        self.label21.set_justify(Gtk.Justification.CENTER)
+        vbox_right.pack_start(self.label21, True, True, 0)
+
+        self.add(hbox)
+
+win = Window()
+win.connect("delete-event", Gtk.main_quit)
+# self.fullscreen()
+win.show_all()
+Gtk.main()
