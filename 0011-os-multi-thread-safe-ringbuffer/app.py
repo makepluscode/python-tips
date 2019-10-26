24c8f06e71dcb71b15951c98a7bb0d5fc2b8474c
makepluscode <39910774+makepluscode@users.noreply.github.com>
2019-10-26 21:11:04 +0900

    Update app.py

--- 0011-os-multi-thread-safe-ringbuffer/app.py
+++ 0011-os-multi-thread-safe-ringbuffer/app.py
@@ -5,34 +5,69 @@ import threading
 
 from queue import Queue
 from threading import Lock
-from periphery import SPI
 
-from logger import *
+class RingBuffer:
+    _instance = None
 
-class RingBuffer(object):
+    @classmethod
+    def _getInstance(cls):
+        return cls._instance
+
+    @classmethod
+    def instance(cls, *args, **kargs):
+        cls._instance = cls(*args, **kargs)
+        cls.instance = cls._getInstance
+        return cls._instance
+
     def __init__(self):
-        self.ringbuffer = []
-        self.lock = Lock()
+        self._buffer = []
+        self._lock = Lock()
 
-    # the class should be a singleton
-    def __new__(self):
-        if not hasattr(self, 'instance'):
-            self.instance = super( RingBuffer, self).__new__(self)
-            return self.instance
+    def get_instance(self):
+        return self.instance
 
     def add_byte(self, byte):
-        self.ringbuffer.append(byte)
+        with self._lock:
+            self._buffer.append(byte)
 
-    def get_byte(self, byte):
-        ret = 0
-        if self.get_size() > 0:
-            self.ringbuffer.pop()
-        else:
-            ret = -1
-        return ret
+    def get_byte(self):
+        byte = 0
+        with self._lock:
+            if len(self._buffer) > 0:
+                byte = self._buffer.pop(0)
+            else:
+                byte = -1
+        return byte
 
-    def get_size(self):
-        return len(self.ringbuffer)
+    def get_byte_8(self):
+        byte_8 = []
+        with self._lock:
+            if len(self._buffer) >= 8:
+                for i in range(8):
+                    byte_8.append(self._buffer.pop(0))
+        return byte_8
+
+    def get_len(self):
+        length = 0
+        with self._lock:
+            length = len(self._buffer)
+        return length
 
     def clear(self):
-        self.ringbuffer.clear()
+        with self._lock:
+            self._buffer.clear()
+
+RingBuffer.instance().add_byte(0x1)
+RingBuffer.instance().add_byte(0x2)
+RingBuffer.instance().add_byte(0x3)
+RingBuffer.instance().add_byte(0x4)
+RingBuffer.instance().add_byte(0x5)
+RingBuffer.instance().add_byte(0x6)
+RingBuffer.instance().add_byte(0x7)
+RingBuffer.instance().add_byte(0x8)
+
+print(RingBuffer.instance().get_len())
+print(RingBuffer.instance().get_byte())
+print(RingBuffer.instance().get_len())
+print(RingBuffer.instance().get_byte_8())
+print(RingBuffer.instance().get_len())
